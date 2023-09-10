6916f60cbf7d7d215904745a5edebf9dc17b94c6
João Victor da Silva Sales <56794256+VictorSilvaaa@users.noreply.github.com>
2023-09-10 16:05:16 -0300

    Update web.php

--- sistema_academico/routes/web.php
+++ sistema_academico/routes/web.php
@@ -29,61 +29,64 @@ use App\Http\Controllers\Student\EditStudentsController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', function () {
+
+$url_base = 'sistema_academico';
+
+Route::get("$url_base/", function () {
     return view('home');
 });
 
-Route::get('/login', function () {
+Route::get("$url_base/login", function () {
     return view('auth.login');
 })->name('login');
 
-Route::post('/login', [LoginController::class, 'authenticate']);
-Route::post('/logout', 
+Route::post("$url_base/login", [LoginController::class, 'authenticate']);
+Route::post("$url_base/logout", 
     [LoginController::class, 'logout']
 )->middleware('auth')->name('logout');
 
-Route::get('/dashboard', 
+Route::get("$url_base/dashboard", 
     [DashboardController::class, 'test']
 )->middleware('auth');
 
-
-Route::middleware(['auth', 'admin'])->group(function () {
-    Route::get('/administrar-escolas', [ManagerSchoolsController::class, 'view'])->name('adm-escola');
+Route::middleware(['auth', 'admin'])->group(function () use ($url_base) {
     
-    Route::get('/cadastrar-escola', [InsertSchoolsController::class, 'view']);
-    Route::post('/cadastrar-escola', [InsertSchoolsController::class, 'store']);
+    Route::get("$url_base/administrar-escolas", [ManagerSchoolsController::class, 'view'])->name('adm-escola');
     
-    Route::get('/atualizar-escola/{id}', [EditSchoolsController::class, 'view']);
-    Route::post('/atualizar-escola/{id}', [EditSchoolsController::class, 'edit']);
+    Route::get("$url_base/cadastrar-escola", [InsertSchoolsController::class, 'view']);
+    Route::post("$url_base/cadastrar-escola", [InsertSchoolsController::class, 'store']);
     
-    Route::get('/excluir-escola/{id}', [DeleteSchoolsController::class, 'view']);
-    Route::delete('/excluir-escola/{id}', [DeleteSchoolsController::class, 'edit']);
+    Route::get("$url_base/atualizar-escola/{id}", [EditSchoolsController::class, 'view']);
+    Route::post("$url_base/atualizar-escola/{id}", [EditSchoolsController::class, 'edit']);
+    
+    Route::get("$url_base/excluir-escola/{id}", [DeleteSchoolsController::class, 'view']);
+    Route::delete("$url_base/excluir-escola/{id}", [DeleteSchoolsController::class, 'edit']);
 });
 
-Route::middleware(['auth', 'admin'])->group(function () {
-    Route::get('/administrar-estudantes', [ManagerStudentsController::class, 'view'])->name('adm-estudante');
+Route::middleware(['auth', 'admin'])->group(function () use ($url_base) {
+    Route::get("$url_base/administrar-estudantes", [ManagerStudentsController::class, 'view'])->name('adm-estudante');
     
-    Route::get('/cadastrar-estudante', [InsertStudentsController::class, 'view']);
-    Route::post('/cadastrar-estudante', [InsertStudentsController::class, 'store']);
+    Route::get("$url_base/cadastrar-estudante", [InsertStudentsController::class, 'view']);
+    Route::post("$url_base/cadastrar-estudante", [InsertStudentsController::class, 'store']);
     
-    Route::get('/atualizar-estudante/{id}', [EditStudentsController::class, 'view']);
-    Route::post('/atualizar-estudante/{id}', [EditStudentsController::class, 'edit']);
+    Route::get("$url_base/atualizar-estudante/{id}", [EditStudentsController::class, 'view']);
+    Route::post("$url_base/atualizar-estudante/{id}", [EditStudentsController::class, 'edit']);
     
-    Route::get('/excluir-estudante/{id}', [DeleteStudentsController::class, 'view']);
-    Route::delete('/excluir-estudante/{id}', [DeleteStudentsController::class, 'edit']);
+    Route::get("$url_base/excluir-estudante/{id}", [DeleteStudentsController::class, 'view']);
+    Route::delete("$url_base/excluir-estudante/{id}", [DeleteStudentsController::class, 'edit']);
 });
 
-Route::middleware(['auth', 'admin'])->group(function () {
-    Route::get('/administrar-professores', [ManagerTeachersController::class, 'view'])->name('adm-professor');
+Route::middleware(['auth', 'admin'])->group(function () use ($url_base) {
+    Route::get("$url_base/administrar-professores", [ManagerTeachersController::class, 'view'])->name('adm-professor');
 
-    Route::get('/cadastrar-professor', [InsertTeachersController::class, 'view']);
-    Route::post('/cadastrar-professor', [InsertTeachersController::class, 'store']);
+    Route::get("$url_base/cadastrar-professor", [InsertTeachersController::class, 'view']);
+    Route::post("$url_base/cadastrar-professor", [InsertTeachersController::class, 'store']);
     
-    Route::get('/atualizar-professor/{id}', [EditTeachersController::class, 'view']);
-    Route::post('/atualizar-professor/{id}', [EditTeachersController::class, 'edit']);
+    Route::get("$url_base/atualizar-professor/{id}", [EditTeachersController::class, 'view']);
+    Route::post("$url_base/atualizar-professor/{id}", [EditTeachersController::class, 'edit']);
     
-    Route::get('/excluir-professor/{id}', [DeleteTeachersController::class, 'view']);
-    Route::delete('/excluir-professor/{id}', [DeleteTeachersController::class, 'edit']);
+    Route::get("$url_base/excluir-professor/{id}", [DeleteTeachersController::class, 'view']);
+    Route::delete("$url_base/excluir-professor/{id}", [DeleteTeachersController::class, 'edit']);
 });
 
 
